e3cb9f536cc3a2eaa3df57eaa941a33e6c69167c
CReMV <chrisvasilas1993@yahoo.gr>
2016-12-12 11:10:15 +0200

    Project Files

---
+++ MainWindow.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TopPanel = New System.Windows.Forms.Panel()
+        Me.DrawCardsButton = New System.Windows.Forms.Button()
+        Me.NameBox = New System.Windows.Forms.TextBox()
+        Me.BottomPanel = New System.Windows.Forms.Panel()
+        Me.ProphecyBox = New System.Windows.Forms.Label()
+        Me.MiddlePanel = New System.Windows.Forms.Panel()
+        Me.MiddleCard = New System.Windows.Forms.PictureBox()
+        Me.RightCard = New System.Windows.Forms.PictureBox()
+        Me.LeftCard = New System.Windows.Forms.PictureBox()
+        Me.TopPanel.SuspendLayout()
+        Me.BottomPanel.SuspendLayout()
+        Me.MiddlePanel.SuspendLayout()
+        CType(Me.MiddleCard, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RightCard, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LeftCard, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TopPanel
+        '
+        Me.TopPanel.BackColor = System.Drawing.Color.Transparent
+        Me.TopPanel.Controls.Add(Me.DrawCardsButton)
+        Me.TopPanel.Controls.Add(Me.NameBox)
+        Me.TopPanel.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TopPanel.Location = New System.Drawing.Point(0, 0)
+        Me.TopPanel.Name = "TopPanel"
+        Me.TopPanel.Size = New System.Drawing.Size(1008, 37)
+        Me.TopPanel.TabIndex = 0
+        '
+        'DrawCardsButton
+        '
+        Me.DrawCardsButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(161, Byte))
+        Me.DrawCardsButton.Location = New System.Drawing.Point(930, 4)
+        Me.DrawCardsButton.Name = "DrawCardsButton"
+        Me.DrawCardsButton.Size = New System.Drawing.Size(75, 30)
+        Me.DrawCardsButton.TabIndex = 1
+        Me.DrawCardsButton.Text = "Draw"
+        Me.DrawCardsButton.UseVisualStyleBackColor = True
+        '
+        'NameBox
+        '
+        Me.NameBox.Font = New System.Drawing.Font("Linux Libertine G", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NameBox.Location = New System.Drawing.Point(4, 4)
+        Me.NameBox.Multiline = True
+        Me.NameBox.Name = "NameBox"
+        Me.NameBox.Size = New System.Drawing.Size(131, 30)
+        Me.NameBox.TabIndex = 0
+        Me.NameBox.Text = "Name"
+        '
+        'BottomPanel
+        '
+        Me.BottomPanel.BackColor = System.Drawing.Color.Transparent
+        Me.BottomPanel.Controls.Add(Me.ProphecyBox)
+        Me.BottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.BottomPanel.Location = New System.Drawing.Point(0, 597)
+        Me.BottomPanel.Name = "BottomPanel"
+        Me.BottomPanel.Size = New System.Drawing.Size(1008, 133)
+        Me.BottomPanel.TabIndex = 2
+        '
+        'ProphecyBox
+        '
+        Me.ProphecyBox.BackColor = System.Drawing.Color.Transparent
+        Me.ProphecyBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ProphecyBox.Font = New System.Drawing.Font("Palatino Linotype", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ProphecyBox.ForeColor = System.Drawing.Color.Azure
+        Me.ProphecyBox.Location = New System.Drawing.Point(0, 0)
+        Me.ProphecyBox.Margin = New System.Windows.Forms.Padding(10)
+        Me.ProphecyBox.Name = "ProphecyBox"
+        Me.ProphecyBox.Size = New System.Drawing.Size(1008, 133)
+        Me.ProphecyBox.TabIndex = 0
+        Me.ProphecyBox.Text = "Label1"
+        Me.ProphecyBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'MiddlePanel
+        '
+        Me.MiddlePanel.BackColor = System.Drawing.Color.Transparent
+        Me.MiddlePanel.Controls.Add(Me.MiddleCard)
+        Me.MiddlePanel.Controls.Add(Me.RightCard)
+        Me.MiddlePanel.Controls.Add(Me.LeftCard)
+        Me.MiddlePanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MiddlePanel.Location = New System.Drawing.Point(0, 37)
+        Me.MiddlePanel.Name = "MiddlePanel"
+        Me.MiddlePanel.Size = New System.Drawing.Size(1008, 560)
+        Me.MiddlePanel.TabIndex = 3
+        '
+        'MiddleCard
+        '
+        Me.MiddleCard.BackColor = System.Drawing.Color.Transparent
+        Me.MiddleCard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.MiddleCard.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MiddleCard.Location = New System.Drawing.Point(336, 0)
+        Me.MiddleCard.Margin = New System.Windows.Forms.Padding(0)
+        Me.MiddleCard.Name = "MiddleCard"
+        Me.MiddleCard.Size = New System.Drawing.Size(336, 560)
+        Me.MiddleCard.TabIndex = 6
+        Me.MiddleCard.TabStop = False
+        '
+        'RightCard
+        '
+        Me.RightCard.BackColor = System.Drawing.Color.Transparent
+        Me.RightCard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.RightCard.Dock = System.Windows.Forms.DockStyle.Right
+        Me.RightCard.Location = New System.Drawing.Point(672, 0)
+        Me.RightCard.Margin = New System.Windows.Forms.Padding(0)
+        Me.RightCard.Name = "RightCard"
+        Me.RightCard.Size = New System.Drawing.Size(336, 560)
+        Me.RightCard.TabIndex = 5
+        Me.RightCard.TabStop = False
+        '
+        'LeftCard
+        '
+        Me.LeftCard.BackColor = System.Drawing.Color.Transparent
+        Me.LeftCard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.LeftCard.Dock = System.Windows.Forms.DockStyle.Left
+        Me.LeftCard.Location = New System.Drawing.Point(0, 0)
+        Me.LeftCard.Margin = New System.Windows.Forms.Padding(0)
+        Me.LeftCard.Name = "LeftCard"
+        Me.LeftCard.Size = New System.Drawing.Size(336, 560)
+        Me.LeftCard.TabIndex = 4
+        Me.LeftCard.TabStop = False
+        '
+        'MainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.TarotProphesizer.My.Resources.Resources.Background
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1008, 730)
+        Me.Controls.Add(Me.MiddlePanel)
+        Me.Controls.Add(Me.BottomPanel)
+        Me.Controls.Add(Me.TopPanel)
+        Me.Name = "MainWindow"
+        Me.Text = "The Card Oracle"
+        Me.TopPanel.ResumeLayout(False)
+        Me.TopPanel.PerformLayout()
+        Me.BottomPanel.ResumeLayout(False)
+        Me.MiddlePanel.ResumeLayout(False)
+        CType(Me.MiddleCard, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RightCard, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LeftCard, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TopPanel As Panel
+    Friend WithEvents BottomPanel As Panel
+    Friend WithEvents NameBox As TextBox
+    Friend WithEvents DrawCardsButton As Button
+    Friend WithEvents ProphecyBox As Label
+    Friend WithEvents MiddlePanel As Panel
+    Friend WithEvents MiddleCard As PictureBox
+    Friend WithEvents RightCard As PictureBox
+    Friend WithEvents LeftCard As PictureBox
+End Class
---
+++ MainWindow.vb
@@ -0,0 +1,158 @@
+﻿Public Class MainWindow
+    'Δεν είναι δικό μου κομμάτι αυτό φυσικά αλλά χρειαζόμουν αλλαγή στα μεγέθη μαζί με το παράθυρο και το Dock και Anchor δεν με κάλυπταν
+    Dim rs As New InputResizer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rs.FindAllControls(Me)
+    End Sub
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        rs.ResizeAllControls(Me)
+    End Sub
+    Public Class InputResizer
+
+        '----------------------------------------------------------
+        ' ControlInfo
+        ' Structure of original state of all processed controls
+        '----------------------------------------------------------
+        Private Structure ControlInfo
+            Public name As String
+            Public parentName As String
+            Public leftOffsetPercent As Double
+            Public topOffsetPercent As Double
+            Public heightPercent As Double
+            Public originalHeight As Integer
+            Public originalWidth As Integer
+            Public widthPercent As Double
+            Public originalFontSize As Single
+        End Structure
+
+        '-------------------------------------------------------------------------
+        ' ctrlDict
+        ' Dictionary of (control name, control info) for all processed controls
+        '-------------------------------------------------------------------------
+        Private ctrlDict As Dictionary(Of String, ControlInfo) = New Dictionary(Of String, ControlInfo)
+
+        '----------------------------------------------------------------------------------------
+        ' FindAllControls
+        ' Recursive function to process all controls contained in the initially passed
+        ' control container and store it in the Control dictionary
+        '----------------------------------------------------------------------------------------
+        Public Sub FindAllControls(thisCtrl As Control)
+
+            '-- If the current control has a parent, store all original relative position
+            '-- and size information in the dictionary.
+            '-- Recursively call FindAllControls for each control contained in the
+            '-- current Control
+            For Each ctl As Control In thisCtrl.Controls
+                Try
+                    If Not IsNothing(ctl.Parent) Then
+                        Dim parentHeight = ctl.Parent.Height
+                        Dim parentWidth = ctl.Parent.Width
+
+                        Dim c As New ControlInfo
+                        c.name = ctl.Name
+                        c.parentName = ctl.Parent.Name
+                        c.topOffsetPercent = Convert.ToDouble(ctl.Top) / Convert.ToDouble(parentHeight)
+                        c.leftOffsetPercent = Convert.ToDouble(ctl.Left) / Convert.ToDouble(parentWidth)
+                        c.heightPercent = Convert.ToDouble(ctl.Height) / Convert.ToDouble(parentHeight)
+                        c.widthPercent = Convert.ToDouble(ctl.Width) / Convert.ToDouble(parentWidth)
+                        c.originalFontSize = ctl.Font.Size
+                        c.originalHeight = ctl.Height
+                        c.originalWidth = ctl.Width
+                        ctrlDict.Add(c.name, c)
+                    End If
+
+                Catch ex As Exception
+                    Debug.Print(ex.Message)
+                End Try
+
+                If ctl.Controls.Count > 0 Then
+                    FindAllControls(ctl)
+                End If
+
+            Next '-- For Each
+
+        End Sub
+
+        '----------------------------------------------------------------------------------------
+        ' ResizeAllControls
+        ' Recursive function to resize and reposition all controls contained in the Control
+        ' dictionary
+        '----------------------------------------------------------------------------------------
+        Public Sub ResizeAllControls(thisCtrl As Control)
+
+            Dim fontRatioW As Single
+            Dim fontRatioH As Single
+            Dim fontRatio As Single
+            Dim f As Font
+
+            '-- Resize and reposition all controls in the passed control
+            For Each ctl As Control In thisCtrl.Controls
+                Try
+                    If Not IsNothing(ctl.Parent) Then
+                        Dim parentHeight = ctl.Parent.Height
+                        Dim parentWidth = ctl.Parent.Width
+
+                        Dim c As New ControlInfo
+
+                        Dim ret As Boolean = False
+                        Try
+                            '-- Get the current control's info from the control info dictionary
+                            ret = ctrlDict.TryGetValue(ctl.Name, c)
+
+                            '-- If found, adjust the current control based on control relative
+                            '-- size and position information stored in the dictionary
+                            If (ret) Then
+                                '-- Size
+                                ctl.Width = Int(parentWidth * c.widthPercent)
+                                ctl.Height = Int(parentHeight * c.heightPercent)
+
+                                '-- Position
+                                ctl.Top = Int(parentHeight * c.topOffsetPercent)
+                                ctl.Left = Int(parentWidth * c.leftOffsetPercent)
+
+                                '-- Font
+                                f = ctl.Font
+                                fontRatioW = ctl.Width / c.originalWidth
+                                fontRatioH = ctl.Height / c.originalHeight
+                                fontRatio = (fontRatioW +
+                                fontRatioH) / 2 '-- average change in control Height and Width
+                                ctl.Font = New Font(f.FontFamily,
+                                c.originalFontSize * fontRatio, f.Style)
+
+                            End If
+                        Catch
+                        End Try
+                    End If
+                Catch ex As Exception
+                End Try
+
+                '-- Recursive call for controls contained in the current control
+                If ctl.Controls.Count > 0 Then
+                    ResizeAllControls(ctl)
+                End If
+
+            Next '-- For Each
+        End Sub
+    End Class
+    'Εδώ τελειώνει ο ξένος κώδικας
+    Private Sub NameBoxClick(sender As Object, e As EventArgs) Handles NameBox.MouseClick
+        NameBox.Text = ""
+    End Sub
+    Private Sub DrawCardsButton_Click(sender As Object, e As EventArgs) Handles DrawCardsButton.Click
+        Dim CardDraw1, CardDraw2, CardDraw3 As New Integer
+        Dim Randomizer As New Random
+        Dim name As String
+        name = NameBox.Text
+        ProphecyBox.Text = name
+        CardDraw1 = Randomizer.Next(1, 77)
+        LeftCard.BackgroundImage = My.Resources.ResourceManager.GetObject("Card" & CardDraw1.ToString)
+        Do
+            CardDraw2 = Randomizer.Next(1, 77)
+        Loop Until (CardDraw2 <> CardDraw1)
+        MiddleCard.BackgroundImage = My.Resources.ResourceManager.GetObject("Card" & CardDraw2.ToString)
+        Do
+            CardDraw3 = Randomizer.Next(1, 77)
+        Loop Until (CardDraw3 <> CardDraw1 And CardDraw3 <> CardDraw2)
+        RightCard.BackgroundImage = My.Resources.ResourceManager.GetObject("Card" & CardDraw3.ToString)
+    End Sub
+End Class
